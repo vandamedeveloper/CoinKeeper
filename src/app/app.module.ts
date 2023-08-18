caebfb9eeae9ce8bad23a3e9a2e11f1eaf585797
Vandame <vandame.hrustic-amirovic-ext@keolis.com>
2023-08-18 16:16:22 +0200

    Added firebase in the project and update tsconfig.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Firebase services + environment module
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from './environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBMfIX9w1OnAEgDYkiW66u_d0kBLW_NsjQ",
+        authDomain: "coinkeeper-9f237.firebaseapp.com",
+        projectId: "coinkeeper-9f237",
+        storageBucket: "coinkeeper-9f237.appspot.com",
+        messagingSenderId: "812819873950",
+        appId: "1:812819873950:web:29c3526a7e1dc815ff7b8f"
+    }
+  };
+  
